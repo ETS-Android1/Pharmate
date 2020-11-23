b5813f66e42e6590dada7f832be8629b0b53d04a
SebnemAcar <sbnmacar0@gmail.com>
2020-11-23 22:34:04 +0300

    SplashScreen yapıldı.(Tasarım daha sonra değiştirilir diye splash ekranını basit yaptım daha sonra farklı yaparız)

---
+++ app/src/main/java/com/example/pharmate/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.pharmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import com.google.android.material.animation.AnimationUtils;
+
+public class SplashScreen extends AppCompatActivity {
+public static int TIME_OUT=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },TIME_OUT);
+
+    }
+}

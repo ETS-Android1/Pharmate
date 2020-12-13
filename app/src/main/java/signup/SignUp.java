34f32906479d23c7311e4bc23891d6a2ceab93f6
SebnemAcar <sbnmacar0@gmail.com>
2020-12-13 13:48:12 +0300

    user için validation(gradle.build app dosyasına yeni bir implementation daha eklendi)

--- app/src/main/java/signup/SignUp.java
+++ app/src/main/java/signup/SignUp.java
@@ -1,38 +1,66 @@
 package signup;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.basgeekball.awesomevalidation.utility.custom.SimpleCustomValidation;
 import com.example.pharmate.MainActivity;
 import com.example.pharmate.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.common.collect.Range;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Locale;
 
 import fragments.Frag1;
+import medicine.UploadMedicine;
 import models.UserClass;
+import users.PersonalInformation;
 
 public class SignUp extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+    private FirebaseFirestore firebaseFirestore;
+    AwesomeValidation awesomeValidation;
+
+    private static final String TAG = "SignUp";
+
+    private DatePickerDialog.OnDateSetListener nOnDateSetListener;
 
     EditText emailText, passwordText,userType, name, userSurname, userTurkishID, userContact, userAddress, userBirthDate;
-    private FirebaseFirestore firebaseFirestore;
+    Button signUpClickButton;
+
 
 
     @Override
@@ -40,8 +68,11 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        firebaseFirestore = FirebaseFirestore.getInstance();
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
 
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
         firebaseAuth = FirebaseAuth.getInstance();
         emailText = findViewById(R.id.signUpEmailText);
         passwordText = findViewById(R.id.signUpPasswordText);
@@ -52,85 +83,123 @@ public class SignUp extends AppCompatActivity {
         userContact = findViewById(R.id.personContactText);
         userAddress = findViewById(R.id.personAddressText);
         userBirthDate = findViewById(R.id.birthDateText);
+        signUpClickButton=findViewById(R.id.signUpClickButton);
+
+        userBirthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(SignUp.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        nOnDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        nOnDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+                String date = month + "/" + day + "/" + year;
+                userBirthDate.setText(date);
+            }
+        };
+
+        String regexpassword = "(?=.*[a-z])(?=.*[A-Z])(?=.*[\\\\d])(?=.*[~`!@#\\\\$%\\\\^&\\\\*\\\\(\\\\)\\\\-_\\\\+=\\\\{\\\\}\\\\[\\\\]\\\\|\\\\;:\\\"<>,./\\\\?]).{8,}";
+        awesomeValidation.addValidation(SignUp.this, R.id.signUpPasswordText,"[a-z][A-Z][1-9]{6,}+" , R.string.passworderror);
+        awesomeValidation.addValidation(SignUp.this, R.id.signUpPasswordText2, R.id.signInPasswordText, R.string.passworderror);
+        awesomeValidation.addValidation(SignUp.this,R.id.turkishIdText,"[1-9]{11}+",R.string.iderror);
+        awesomeValidation.addValidation(SignUp.this, R.id.signUpEmailText, android.util.Patterns.EMAIL_ADDRESS, R.string.emailerror);
+        awesomeValidation.addValidation(SignUp.this, R.id.personNameText, "[a-zA-Z\\s]+", R.string.nameerror);
+        awesomeValidation.addValidation(SignUp.this, R.id.personSurnameText, "[a-zA-Z\\s]+", R.string.surnameerror);
+        awesomeValidation.addValidation(SignUp.this, R.id.personContactText, RegexTemplate.TELEPHONE, R.string.mobileerror);
+
+
 
         // girilen inputları kontrol edicez.
     }
     public void signUpClick (View view) {
-        System.out.println("button pressed");
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        String nameText = name.getText().toString();
-        String userSurnameText = userSurname.getText().toString();
-        String userTurkishIDText = userTurkishID.getText().toString();
-        String userAddressText = userAddress.getText().toString();
-        String userContactText = userContact.getText().toString();
-        String userTypeText = userType.getText().toString();
-        String userBirthDayText = userBirthDate.getText().toString();
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
-        // bir ust satirda kullanicidan aldigimiz sifreyi asagidaki firebase'e gondermeden once
-        // validation islemlerini yapacagiz
+        if (awesomeValidation.validate()) {
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+            String nameText = name.getText().toString();
+            String userSurnameText = userSurname.getText().toString();
+            String userTurkishIDText = userTurkishID.getText().toString();
+            String userAddressText = userAddress.getText().toString();
+            String userContactText = userContact.getText().toString();
+            String userTypeText = userType.getText().toString();
+            String userBirthDayText = userBirthDate.getText().toString();
+            String email = emailText.getText().toString();
+            String password = passwordText.getText().toString();
+            // bir ust satirda kullanicidan aldigimiz sifreyi asagidaki firebase'e gondermeden once
+
+            // validation islemlerini yapacagiz
 
 
-        firebaseAuth.createUserWithEmailAndPassword(email,password)
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        firebaseAuth.getCurrentUser().sendEmailVerification()
-                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if(task.isSuccessful()){
-                                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                                    .setDisplayName(nameText+userSurnameText)
-                                                    .build();
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            firebaseAuth.getCurrentUser().sendEmailVerification()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                                        .setDisplayName(nameText + userSurnameText)
+                                                        .build();
 
-                                            firebaseUser.updateProfile(profileUpdates)
-                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                        @Override
-                                                        public void onComplete(@NonNull Task<Void> task) {
-                                                            if (task.isSuccessful()) {
-                                                                DocumentSnapshot document = null;
-                                                                UserClass userClass = document.toObject(UserClass.class);
-                                                                System.out.println("Task Successful");
+                                                firebaseUser.updateProfile(profileUpdates)
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    System.out.println("Task Successful");
+                                                                }
                                                             }
-                                                        }
-                                                    });
+                                                        });
 
-                                            UserClass userClassToAdd = new UserClass(nameText, userSurnameText, userTurkishIDText,userAddressText,userContactText,userBirthDayText,null);
-                                            String userEmail = firebaseUser.getEmail();
-                                            String userID = firebaseUser.getUid();
+                                                UserClass userClassToAdd = new UserClass(nameText, userSurnameText, userTurkishIDText, userAddressText, userContactText, userBirthDayText, null);
+                                                String id = firebaseUser.getUid();
 
-                                            HashMap<String, Object> postUserData = new HashMap<>();
+                                                HashMap<String, Object> postUserData = new HashMap<>();
 
 
-                                            postUserData.put("type", userClassToAdd.getType());
-                                            postUserData.put("name", userClassToAdd.getName());
-                                            postUserData.put("surname", userClassToAdd.getSurname());
-                                            postUserData.put("turkishId", userClassToAdd.getTurkishId());
-                                            postUserData.put("contact", userClassToAdd.getContact());
-                                            postUserData.put("address",userClassToAdd.getAddress());
-                                            postUserData.put("birthDate", userClassToAdd.getBirthdate());
-                                            firebaseFirestore.collection("user").document(userID).set(postUserData);
-                                            Toast.makeText(SignUp.this,"please check your email", Toast.LENGTH_LONG).show();
-                                            Intent intent = new Intent(SignUp.this, MainActivity.class);
-                                            startActivity(intent);
-                                        }else{
-                                            Toast.makeText(SignUp.this, task.getException().getMessage(),
-                                                    Toast.LENGTH_LONG).show(); }
+                                                postUserData.put("type", userClassToAdd.getType());
+                                                postUserData.put("name", userClassToAdd.getName());
+                                                postUserData.put("surname", userClassToAdd.getSurname());
+                                                postUserData.put("turkishId", userClassToAdd.getTurkishId());
+                                                postUserData.put("contact", userClassToAdd.getContact());
+                                                postUserData.put("address", userClassToAdd.getAddress());
+                                                postUserData.put("birthDate", userClassToAdd.getBirthdate());
+                                                firebaseFirestore.collection("userType").document(id).set(postUserData);
+                                                Toast.makeText(SignUp.this, "please check your email", Toast.LENGTH_LONG).show();
+                                                Intent intent = new Intent(SignUp.this, MainActivity.class);
+                                                startActivity(intent);
+                                            } else {
+                                                Toast.makeText(SignUp.this, task.getException().getMessage(),
+                                                        Toast.LENGTH_LONG).show();
+                                            }
 
-                                    }
-                                });
-
-
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(SignUp.this, e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
-            }
-        });
+                                        }
+                                    });
 
 
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(SignUp.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+
+        }
     }
 
 }
--- app/src/main/java/users/PersonalInformation.java
+++ app/src/main/java/users/PersonalInformation.java
@@ -1,15 +1,25 @@
 package users;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.basgeekball.awesomevalidation.utility.custom.SimpleCustomValidation;
 import com.example.pharmate.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.common.collect.Range;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -19,16 +29,31 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Locale;
 
 import models.MedicineClass;
 import models.UserClass;
+import signup.SignUp;
+
+import static android.graphics.Color.*;
+import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
+import static com.basgeekball.awesomevalidation.ValidationStyle.COLORATION;
 
 public class PersonalInformation extends AppCompatActivity {
     private FirebaseStorage firebaseStorage;
     private StorageReference storageReference;
     private FirebaseFirestore firebaseFirestore;
     private FirebaseAuth firebaseAuth;
+    AwesomeValidation awesomeValidation;
+
+
+    private static final String TAG = "PersonalInformation";
+
+    private DatePickerDialog.OnDateSetListener nOnDateSetListener;
 
 
     EditText userType, name, userSurname, userTurkishID, userContact, userAddress, userBirthDate;
@@ -43,6 +68,7 @@ public class PersonalInformation extends AppCompatActivity {
         storageReference = firebaseStorage.getReference();
         firebaseFirestore = FirebaseFirestore.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
 
 //
 //        //defining textFields
@@ -55,52 +81,87 @@ public class PersonalInformation extends AppCompatActivity {
         userBirthDate = findViewById(R.id.birthDateText);
 
 
+        userBirthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(PersonalInformation.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        nOnDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(TRANSPARENT));
+                dialog.show();
+            }
+        });
+        nOnDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+                String date = month + "/" + day + "/" + year;
+                userBirthDate.setText(date);
+            }
+        };
+        String regexusertype = "(?=.*[a-z])(?=.*[A-Z])(?=.*[\\\\d])(?=.*[~`!@#\\\\$%\\\\^&\\\\*\\\\(\\\\)\\\\-_\\\\+=\\\\{\\\\}\\\\[\\\\]\\\\|\\\\;:\\\"<>,./\\\\?]).{8,}";
+        awesomeValidation.addValidation(PersonalInformation.this, R.id.userTypeText,"[a-zA-Z\\s]+", R.string.usernameerror);
+        awesomeValidation.addValidation(PersonalInformation.this,R.id.turkishIdText,"[0-9]{11}+",R.string.iderror);
+        awesomeValidation.addValidation(PersonalInformation.this, R.id.personNameText, "[a-zA-Z\\s]+", R.string.nameerror);
+        awesomeValidation.addValidation(PersonalInformation.this, R.id.personSurnameText, "[a-zA-Z\\s]+", R.string.surnameerror);
+        awesomeValidation.addValidation(PersonalInformation.this, R.id.personContactText, RegexTemplate.TELEPHONE, R.string.mobileerror);
     }
+
 
     public void submitPersonInfoClick(View v) {
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        String nameText = name.getText().toString();
-        String userSurnameText = userSurname.getText().toString();
-        String userTurkishIDText = userTurkishID.getText().toString();
-        String userAddressText = userAddress.getText().toString();
-        String userContactText = userContact.getText().toString();
-        String userTypeText = userType.getText().toString();
-        String userBirthDayText = userBirthDate.getText().toString();
+
+        if (awesomeValidation.validate()) {
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+            String nameText = name.getText().toString();
+            String userSurnameText = userSurname.getText().toString();
+            String userTurkishIDText = userTurkishID.getText().toString();
+            String userAddressText = userAddress.getText().toString();
+            String userContactText = userContact.getText().toString();
+            String userTypeText = userType.getText().toString();
+            String userBirthDayText = userBirthDate.getText().toString();
 
 
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(nameText+userSurnameText)
-                .build();
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(nameText + userSurnameText)
+                    .build();
 
-        firebaseUser.updateProfile(profileUpdates)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = null;
-                            UserClass userClass = document.toObject(UserClass.class);
-                            System.out.println("Task Successful");
+            firebaseUser.updateProfile(profileUpdates)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = null;
+                                UserClass userClass = document.toObject(UserClass.class);
+                                System.out.println("Task Successful");
+                            }
                         }
-                    }
-                });
+                    });
 
-        UserClass userClassToAdd = new UserClass(nameText, userSurnameText, userTurkishIDText,userAddressText,userContactText,userTypeText,userBirthDayText);
-        String userEmail = firebaseUser.getEmail();
-        String userID = firebaseUser.getUid();
+            UserClass userClassToAdd = new UserClass(nameText, userSurnameText, userTurkishIDText, userAddressText, userContactText, userTypeText, userBirthDayText);
+            String userEmail = firebaseUser.getEmail();
+            String userID = firebaseUser.getUid();
 
-        HashMap<String, Object> postUserData = new HashMap<>();
+            HashMap<String, Object> postUserData = new HashMap<>();
 
 
-        postUserData.put("type", userClassToAdd.getType());
-        postUserData.put("name", userClassToAdd.getName());
-        postUserData.put("surname", userClassToAdd.getSurname());
-        postUserData.put("turkishId", userClassToAdd.getTurkishId());
-        postUserData.put("contact", userClassToAdd.getContact());
-        postUserData.put("address",userClassToAdd.getAddress());
-        postUserData.put("birthDate", userClassToAdd.getBirthdate());
-        firebaseFirestore.collection("userType").document(userID).set(postUserData);
+            postUserData.put("type", userClassToAdd.getType());
+            postUserData.put("name", userClassToAdd.getName());
+            postUserData.put("surname", userClassToAdd.getSurname());
+            postUserData.put("turkishId", userClassToAdd.getTurkishId());
+            postUserData.put("contact", userClassToAdd.getContact());
+            postUserData.put("address", userClassToAdd.getAddress());
+            postUserData.put("birthDate", userClassToAdd.getBirthdate());
+            firebaseFirestore.collection("userType").document(userID).set(postUserData);
+        }
     }
-    }
+}
 
 
 

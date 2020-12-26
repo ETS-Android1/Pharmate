56f2a2807067ab34b2eaf9f34f2b2dc2802d0e10
hiusanmaz <hiusanmaz34@gmail.com>
2020-12-26 22:58:53 +0300

    Location icin gerekli metodlar eklendi

--- app/src/main/java/models/OrganizationClass.java
+++ app/src/main/java/models/OrganizationClass.java
@@ -1,23 +1,25 @@
 package models;
 
+import com.google.firebase.firestore.GeoPoint;
+
 public class OrganizationClass {
 
     private String city, email, manager, province;
+    GeoPoint location;
 
 
-    public OrganizationClass(){}
+    public OrganizationClass() {
+    }
 
 
-    public OrganizationClass(String city, String email, String manager, String province) {
+    public OrganizationClass(String city, String email, String manager, String province, GeoPoint location) {
         this.city = city;
         this.email = email;
         this.manager = manager;
         this.province = province;
-
+        this.location = location;
     }
 
-    public OrganizationClass(String orgtext, String orgaddresstext, String orgcontact) {
-    }
 
     public String getCity() {
         return city;
@@ -31,13 +33,25 @@ public class OrganizationClass {
         return email;
     }
 
-    public void setEmail(String email) { this.email = city; }
+    public GeoPoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(GeoPoint location) {
+        this.location = location;
+    }
+
+    public void setEmail(String email) {
+        this.email = city;
+    }
 
     public String getManager() {
         return manager;
     }
 
-    public void setManager(String manager) { this.manager = manager; }
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
 
     public String getProvince() {
         return province;
--- app/src/main/java/signup/SignUpOrg.java
+++ app/src/main/java/signup/SignUpOrg.java
@@ -23,6 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -30,6 +31,7 @@ import java.util.HashMap;
 
 import fragments.Choose;
 import location.LocationTracker;
+import models.OrganizationClass;
 
 public class SignUpOrg extends AppCompatActivity {
     AwesomeValidation awesomeValidation;
@@ -101,11 +103,17 @@ public class SignUpOrg extends AppCompatActivity {
 
                                                 String id = firebaseUser.getUid();
                                                 LatLng organizationLocation = getLocation();
+                                                GeoPoint location = new GeoPoint(organizationLocation.latitude, organizationLocation.longitude);
+
+                                                OrganizationClass organizationClass = new OrganizationClass(orgaddresstext, email, orgtext, orgcontact, location);
                                                 HashMap<String, Object> postUserData = new HashMap<>();
-                                                postUserData.put("manager", orgtext);
-                                                postUserData.put("province", orgcontact);
-                                                postUserData.put("city", orgaddresstext);
-                                                postUserData.put("location", organizationLocation);
+
+                                                postUserData.put("manager", organizationClass.getManager());
+                                                postUserData.put("province", organizationClass.getProvince());
+                                                postUserData.put("city", organizationClass.getCity());
+                                                postUserData.put("location", organizationClass.getLocation());
+                                                postUserData.put("email", organizationClass.getEmail());
+
                                                 firebaseFirestore.collection("organization").document(id).set(postUserData);
                                                 Toast.makeText(SignUpOrg.this, "please check your email", Toast.LENGTH_LONG).show();
                                                 Intent intent = new Intent(SignUpOrg.this, Choose.class);

6c2a2ba044ad2e86b4b04cf9b849ab0902f1ef97
hiusanmaz <hiusanmaz34@gmail.com>
2020-12-27 20:09:19 +0300

    LocationOptions duzenlendi

--- app/src/main/java/users/PersonalInformation.java
+++ app/src/main/java/users/PersonalInformation.java
@@ -2,7 +2,6 @@ package users;
 
 import android.Manifest;
 import android.app.DatePickerDialog;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -18,7 +17,6 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,24 +26,16 @@ import androidx.core.content.ContextCompat;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
-import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 import com.example.pharmate.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
@@ -55,31 +45,25 @@ import java.util.HashMap;
 import java.util.UUID;
 
 import homepage.HomePage;
-import models.UserClass;
 
 import static android.graphics.Color.TRANSPARENT;
 
 public class PersonalInformation extends AppCompatActivity {
+    private static final String TAG = "PersonalInformation";
+    public Uri imageUri;
+    AwesomeValidation awesomeValidation;
+    EditText name, userSurname, userTurkishID, userContact, userAddress, userBirthDate;
+    String nameperson, surnameperson, turkisIdperson, contactperson, birthdateperson, addressperson;
+    Button update;
+    ImageView picture;
+    Bitmap selectedImage;
+    String userId;
     private FirebaseStorage firebaseStorage;
     private StorageReference storageReference;
     private FirebaseFirestore firebaseFirestore;
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
-    AwesomeValidation awesomeValidation;
-
-
-    private static final String TAG = "PersonalInformation";
-
     private DatePickerDialog.OnDateSetListener nOnDateSetListener;
-
-
-    EditText name, userSurname, userTurkishID, userContact, userAddress, userBirthDate;
-    String nameperson, surnameperson, turkisIdperson, contactperson, birthdateperson, addressperson;
-    Button update;
-    ImageView picture;
-    public Uri imageUri;
-    Bitmap selectedImage;
-    String userId;
 
     // KULLANICININ BU FORMU DOLDURDUĞUNU UYGULAMA BOYUNCA KONTROL EDİLMESİ GEREKİYOR.
     @Override
@@ -95,14 +79,14 @@ public class PersonalInformation extends AppCompatActivity {
 
 //
 //        //defining
-        picture=findViewById(R.id.pictureId);
+        picture = findViewById(R.id.pictureId);
         name = findViewById(R.id.personNameText);
         userSurname = findViewById(R.id.personSurnameText);
         userTurkishID = findViewById(R.id.turkishIdText);
         userContact = findViewById(R.id.personContactText);
         userAddress = findViewById(R.id.personAddressText);
         userBirthDate = findViewById(R.id.birthDateText);
-        update=findViewById(R.id.button3);
+        update = findViewById(R.id.button3);
         userBirthDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -129,12 +113,12 @@ public class PersonalInformation extends AppCompatActivity {
             }
         };
 
-        userId=firebaseAuth.getCurrentUser().getUid();
-        DocumentReference documentReference=firebaseFirestore.collection("user").document(userId);
+        userId = firebaseAuth.getCurrentUser().getUid();
+        DocumentReference documentReference = firebaseFirestore.collection("user").document(userId);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-name.setText(value.getString("name"));
+                name.setText(value.getString("name"));
                 userSurname.setText(value.getString("surname"));
                 userTurkishID.setText(value.getString("turkishId"));
                 userContact.setText(value.getString("contact"));
@@ -152,7 +136,7 @@ name.setText(value.getString("name"));
 
             //universal unique id
             UUID uuid = UUID.randomUUID();
-            final String imageName = "images/" + uuid + ".jpg";
+            final String imageName = "images/" + userId + ".jpg";
 
             storageReference.child(imageName).putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
@@ -172,52 +156,36 @@ name.setText(value.getString("name"));
 
                             String Name = name.getText().toString();
                             String surName = userSurname.getText().toString();
-                            String id = userTurkishID.getText().toString();
+                            String turkishId = userTurkishID.getText().toString();
                             String contact = userContact.getText().toString();
                             String address = userAddress.getText().toString();
                             String birthdate = userBirthDate.getText().toString();
 
 
                             HashMap<String, Object> postData = new HashMap<>();
-                            postData.put("name",Name);
-                            postData.put("surname",surName);
-                            postData.put("turkishId",id);
-                            postData.put("contact",contact);
-                            postData.put("address",address);
-                            postData.put("birthDate",birthdate);
+                            postData.put("name", Name);
+                            postData.put("surname", surName);
+                            postData.put("turkishId", turkishId);
+                            postData.put("contact", contact);
+                            postData.put("address", address);
+                            postData.put("birthDate", birthdate);
 
 
-                            firebaseFirestore.collection("user").add(postData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                @Override
-                                public void onSuccess(DocumentReference documentReference) {
+                            firebaseFirestore.collection("user").document(userId).update(postData);
 
-                                    Intent intent = new Intent(PersonalInformation.this, HomePage.class);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                    startActivity(intent);
+                            Intent intent = new Intent(PersonalInformation.this, HomePage.class);
 
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(PersonalInformation.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
-                                }
-                            });
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-
-
+                            startActivity(intent);
+                            finish();
 
                         }
                     });
 
 
                 }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(PersonalInformation.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
-                }
             });
-
         }
 
     }
@@ -229,28 +197,29 @@ name.setText(value.getString("name"));
         if (requestCode == 1) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intentToGallery,2);
+                startActivityForResult(intentToGallery, 2);
             }
         }
 
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
-        if (requestCode == 2 && resultCode == RESULT_OK && data != null ) {
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
 
             imageUri = data.getData();
 
             try {
 
                 if (Build.VERSION.SDK_INT >= 28) {
-                    ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(),imageUri);
+                    ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), imageUri);
                     selectedImage = ImageDecoder.decodeBitmap(source);
                     picture.setImageBitmap(selectedImage);
                 } else {
-                    selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);
+                    selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
                     picture.setImageBitmap(selectedImage);
                 }
 
@@ -263,16 +232,15 @@ name.setText(value.getString("name"));
         }
 
 
-
         super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void selectedImage(View view) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
         } else {
             Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(intentToGallery,2);
+            startActivityForResult(intentToGallery, 2);
         }
     }
 }

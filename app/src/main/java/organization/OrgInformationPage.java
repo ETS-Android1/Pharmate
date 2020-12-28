78b8408c97a6caa78bdf6fc2b79bc87ee8104d50
Halil Usanmaz <33184936+hiusanmaz@users.noreply.github.com>
2020-12-29 01:17:38 +0300

    Revert "gerekli sayfalara progressbar eklendi."

--- app/src/main/java/organization/OrgInformationPage.java
+++ app/src/main/java/organization/OrgInformationPage.java
@@ -2,7 +2,6 @@ package organization;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,9 +9,7 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
-import com.example.pharmate.Loadingbar;
 import com.example.pharmate.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -29,8 +26,6 @@ import com.google.firebase.storage.StorageReference;
 
 import java.util.HashMap;
 
-
-import medicine.UploadMedicine;
 import homepage.HomePage;
 import models.OrganizationClass;
 import users.PersonalInformation;
@@ -41,8 +36,6 @@ public class OrgInformationPage extends AppCompatActivity {
     private FirebaseFirestore firebaseFirestore;
     private FirebaseAuth firebaseAuth;
 
-    Button OrgSubmit;
-    CardView cardView;
     EditText  OrgNameText,OrgContactText,OrgAddressText;
     Button updateInfo;
     String userId;
@@ -50,25 +43,6 @@ public class OrgInformationPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_org_information_page);
-
-        OrgSubmit=findViewById(R.id.OrgSubmit);
-        cardView=findViewById(R.id.cardview);
-        final Loadingbar loadingbar = new Loadingbar(OrgInformationPage.this);
-
-        OrgSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cardView.setVisibility(View.VISIBLE);
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        cardView.setVisibility(View.GONE);
-
-                    }
-                }, 5000);
-            }
-        });
         firebaseStorage = FirebaseStorage.getInstance();
         storageReference = firebaseStorage.getReference();
         firebaseFirestore = FirebaseFirestore.getInstance();
